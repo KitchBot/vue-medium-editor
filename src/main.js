80e2be556f4eeef7373060945651c81c9261e18e
rjrodger <richard@ricebridge.com>
2020-01-25 00:36:07 +0000

    v3.3.1

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import '@voxgig/medium-editor/dist/css/medium-editor.css'
-import '@voxgig/medium-editor/dist/css/themes/beagle.css'
+import '@voxgig/medium-editor/dist/css/themes/voxgig.css'
 
 Vue.config.productionTip = false
 
